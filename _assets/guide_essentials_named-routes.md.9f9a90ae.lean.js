1e013712a8d9083bd9762ba0ac3a590042d02795
joehecn <leanbrown@live.cn>
2020-12-10 00:44:42 +0000

    Deploying to gh-pages from  @ 98ba20eb3f024f4fda093df9daeb340288deb438 🚀

---
+++ _assets/guide_essentials_named-routes.md.9f9a90ae.lean.js
@@ -0,0 +1,1 @@
+import{o as a,e as n,G as s}from"./framework.c00dda38.js";const t='{"title":"Named Routes","description":"","frontmatter":{},"relativePath":"guide/essentials/named-routes.md","lastUpdated":1607561054500.6162}';var e={};const o=s('',10);e.render=function(s,t,e,p,c,r){return a(),n("div",null,[o])};export default e;export{t as __pageData};
